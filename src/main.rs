d7fa041e6e0bdd72238eaf09ed7a1291ead24628
Leonardo Barbosa <leonardobarbosa70@gmail.com>
2024-12-06 00:36:54 -0300

    Guessing Game - Rust Lang Book

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn  main() {
     println!("Guessed number !\n");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100); //Gerador de número pseudoaleatório
 
-    println!("Please input your guess.\n");
+    //println!("The secret number is: {secret_number}\n");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line\n");
+        let mut guess = String::new(); //Variável do tipo String que vai guardar o valor do input do usuário
 
-    println!("You guessed: {}\n", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }; //Converte de String para U32(Unsigned32bits) e continua se houver input fora do padrão
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!\n"),
+            Ordering::Greater => println!("Too big!\n"),
+            Ordering::Equal => {
+                println!("You win!\n");
+                break;
+            }
+        }
+    }
 }
